9300e89c23c1305ee46a25367a6e85b3b109d955
Segev Finer <segev@swimm.io>
2024-02-04 23:17:02 +0200

    Add build:ssr command

--- packages/foo/vite.config.ts
+++ packages/foo/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-      formats: ['es'],
+      formats: ['cjs', 'es'],
     },
   },
 });
